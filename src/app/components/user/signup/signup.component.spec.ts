1735595e4a9cd10df53b5c67e6af8a6739d396d1
Tim <timgomperts@gmail.com>
2024-09-18 17:37:53 +0200

    Funció per els labels

--- src/app/components/user/signup/signup.component.spec.ts
+++ src/app/components/user/signup/signup.component.spec.ts
@@ -5,6 +5,7 @@ import { SignupComponent } from './signup.component';
 describe('SignupComponent', () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,6 +16,7 @@ describe('SignupComponent', () => {
     fixture = TestBed.createComponent(SignupComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
   it('should create', () => {
@@ -22,23 +24,23 @@ describe('SignupComponent', () => {
   });
 
   it('Has title Signup', () => {
-    const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toBe('Signup');
   });
 
   it('Has label for Username', () => {
-    const compiled:HTMLElement = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('label')?.textContent).toBe('Username:');
+    expect(getNthLabelContent(0)).toBe('Username:');
   });
 
   it('Has label for Email', () => {
-    const compiled:HTMLElement = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelectorAll('label')[1]?.textContent).toBe('Email:');
+    expect(getNthLabelContent(1)).toBe('Email:');
   });
 
   it('Has label for Password', () => {
-    const compiled:HTMLElement = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelectorAll('label')[2]?.textContent).toBe('Password:');
-  }); 
+    expect(getNthLabelContent(2)).toBe('Password:');
+  });
+
+  function getNthLabelContent(index: number) {
+    return compiled.querySelectorAll('label')[index]?.textContent;
+  }
 
 });
